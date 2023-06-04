32ab361a1e08f9d94a6acd793acc1375afcbc851
Mahir <mahirgs@code.edu.az>
2023-06-04 08:32:36 +0400

    ViewComponent, Category crud, File upload, Slider

---
+++ Fiorello/Fiorello/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,104 @@
+﻿using Fiorello.Areas.Admin.ViewModels.Slider;
+using Fiorello.Data;
+using Fiorello.Helpers;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiorello_backend.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public SliderController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<SliderVM> sliderList = new();
+
+            List<Slider> sliders = await _context.sliders.ToListAsync();
+
+            foreach (Slider slider in sliders)
+            {
+                SliderVM model = new()
+                {
+                    Id = slider.Id,
+                    Image = slider.Image,
+                    CreateDate = slider.CreatedDate.ToString("dd-MM-yyyy")
+                };
+
+                sliderList.Add(model);
+            }
+
+            return View(sliderList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id is null) return BadRequest();
+
+            Slider dbSlider = await _context.sliders.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (dbSlider is null) return NotFound();
+
+            SliderDetailVM model = new()
+            {
+                Image = dbSlider.Image,
+                CreateDate = dbSlider.CreatedDate.ToString("dd-MM-yyyy"),
+            };
+
+            return View(model);
+
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SliderCreateVM request)
+        {
+            if (!ModelState.IsValid) return View();
+
+            if (!request.Image.CheckFileType("image/")) 
+            {
+                ModelState.AddModelError("Image", "Please select only image file.");
+                return View();
+            }
+
+            if(request.Image.CheckFileSize(200))
+            {
+                ModelState.AddModelError("Image", "Please select under 200KB image");
+                return View();
+            }
+
+            string fileName = Guid.NewGuid().ToString() + "_" + request.Image.FileName;
+
+            await request.Image.SaveFileAsync(fileName, _env.WebRootPath, "img");
+    
+            Slider slider = new()
+            {
+                Image = fileName
+            };
+
+            await _context.AddAsync(slider);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Fiorello/Fiorello/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Fiorello.Data;
+using Fiorello.Models;
+using Fiorello.ViewModels;
+using Fiorello.ViewModels;
+
+namespace Fiorello.ViewComponents
+{
+    public class SliderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public SliderViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<Slider> sliders = await _context.sliders.Where(m => !m.SoftDelete).ToListAsync();
+            SlidersInfo sliderInfo = await _context.slidersInfos.Where(m => !m.SoftDelete).FirstOrDefaultAsync();
+
+            SliderVM sliderVM = new() 
+            {
+                Sliders = sliders,
+                SliderInfo = sliderInfo
+            };
+
+            return await Task.FromResult(View(sliderVM));
+        }
+    }
+}
